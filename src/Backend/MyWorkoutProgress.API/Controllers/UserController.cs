e8e2b852fc280446373f97dbc700b184fefab6b7
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-05-08 16:45:33 -0300

    feat: adding endpoint to register an user

---
+++ src/Backend/MyWorkoutProgress.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWorkoutProgress.Communication.Requests;
+using MyWorkoutProgress.Communication.Responses;
+
+namespace MyWorkoutProgress.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+    public IActionResult Register(RequestRegisterUserJson request)
+    {
+        return Created();
+    }
+}
