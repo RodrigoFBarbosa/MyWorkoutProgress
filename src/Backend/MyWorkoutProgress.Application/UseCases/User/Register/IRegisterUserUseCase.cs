76ffb47afe749f0591648b72fefdd62deb8f2b81
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-05-09 16:54:54 -0300

    feat: adding validation for user's register

---
+++ src/Backend/MyWorkoutProgress.Application/UseCases/User/Register/IRegisterUserUseCase.cs
@@ -0,0 +1,9 @@
+﻿using MyWorkoutProgress.Communication.Requests;
+using MyWorkoutProgress.Communication.Responses;
+
+namespace MyWorkoutProgress.Application.UseCases.User.Register;
+
+public interface IRegisterUserUseCase
+{
+    public Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request);
+}
--- src/Backend/MyWorkoutProgress.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyWorkoutProgress.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,13 +1,25 @@
 ﻿using MyWorkoutProgress.Communication.Requests;
 using MyWorkoutProgress.Communication.Responses;
+using MyWorkoutProgress.Domain.Repositories.User;
+using MyWorkoutProgress.Exceptions;
+using MyWorkoutProgress.Exceptions.ExceptionsBase;
 
 namespace MyWorkoutProgress.Application.UseCases.User.Register;
 
-public class RegisterUserUseCase
+public class RegisterUserUseCase : IRegisterUserUseCase
 {
-    public ResponseRegisteredUserJson Execute (RequestRegisterUserJson request)
+    private readonly IUserWriteOnlyRepository _writeOnlyRepository;
+    private readonly IUserReadOnlyRepository _readOnlyRepository;
+
+    public RegisterUserUseCase(IUserWriteOnlyRepository writeOnlyRepository, IUserReadOnlyRepository readOnlyRepository)
     {
-        // validate request
+        _writeOnlyRepository = writeOnlyRepository;
+        _readOnlyRepository = readOnlyRepository;
+    }
+
+    public async Task<ResponseRegisteredUserJson> Execute (RequestRegisterUserJson request)
+    {
+        await Validate(request);
 
         // mapping entity request
 
@@ -16,5 +28,24 @@ public class RegisterUserUseCase
         // save changes DB
 
         return new ResponseRegisteredUserJson { Name = request.Name, };
+    }
+
+    public async Task Validate(RequestRegisterUserJson request)
+    {
+        var validator = new RegisterUserValidator();
+
+        var result = validator.Validate(request);
+
+        var emailExist = await _readOnlyRepository.ExistActiveUserWithEmail(request.Email);
+
+        if (emailExist)
+            result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, ResourceMessagesException.EMAIL_ALREADY_REGISTERED));
+        
+        if (result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);    
+        }
     }
 }
