78613237f1c553f11d9fe783d04602a6111c8d86
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-05-09 16:52:31 -0300

    feat: adding entity base and user entity

---
+++ src/Backend/MyWorkoutProgress.Domain/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace MyWorkoutProgress.Domain.Entities;
+
+public class EntityBase
+{
+    public int Id { get; set; }
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+}
