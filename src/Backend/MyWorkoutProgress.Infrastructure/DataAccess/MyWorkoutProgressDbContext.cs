4c2c5c0a3c019711bcac9c86450406bc7c7611a2
Rodrigo <rodrigobarbosa.p99@gmail.com>
2025-05-19 16:48:20 -0300

    feat: adding DbContext and UnitOfWork

---
+++ src/Backend/MyWorkoutProgress.Infrastructure/DataAccess/MyWorkoutProgressDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWorkoutProgress.Domain.Entities;
+
+namespace MyWorkoutProgress.Infrastructure.DataAccess;
+
+public class MyWorkoutProgressDbContext : DbContext
+{
+    public MyWorkoutProgressDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyWorkoutProgressDbContext).Assembly);
+    }
+}
---
+++ src/Backend/MyWorkoutProgress.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using MyWorkoutProgress.Domain.Repositories;
+
+namespace MyWorkoutProgress.Infrastructure.DataAccess;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly MyWorkoutProgressDbContext _dbContext;
+
+    public UnitOfWork(MyWorkoutProgressDbContext dbContext) => _dbContext = dbContext;
+
+    public async Task Commit() => await _dbContext.SaveChangesAsync();
+}
